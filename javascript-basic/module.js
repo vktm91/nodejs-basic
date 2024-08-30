61054f4c37c8f0571fb71a468a0fbd4ffbfe3ea0
dadm <vktm91@nate.com>
2024-08-30 16:50:51 +0900

    chore: module import

---
+++ javascript-basic/module.js
@@ -0,0 +1,5 @@
+const { puku, pukuSister} = require('./users');
+const hello = require('./hello');
+
+hello(puku);
+hello(pukuSister);
---
+++ javascript-basic/users.js
@@ -0,0 +1,5 @@
+const puku = "뿌꾸";
+const pukuSister = "뿌꾸언니";
+const pukuMother = "뿌꾸엄마";
+
+module.exports = { puku, pukuSister };
