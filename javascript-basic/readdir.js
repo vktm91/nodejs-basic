b68e1f0064ffb0fcc44d5a1bf3059c9b11a541a0
dadm <vktm91@nate.com>
2024-09-01 18:35:00 +0900

    chore: fs readdir

---
+++ javascript-basic/readdir.js
@@ -0,0 +1,21 @@
+// fs 모듈의 readdir 함수 연습하기
+
+const fs = require("fs");
+
+fs.readdir("./", (err, files) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(files);
+})
+fs.readFile("./example.txt", "utf8", (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+    fs.writerFile("./text.txt", data, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('text.txt is saved');
+    })
+})
