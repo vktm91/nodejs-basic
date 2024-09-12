859f663d7228f9d8c607e187050ef04b6948393a
dadm <vktm91@nate.com>
2024-09-12 19:29:14 +0900

    chore: express2

--- do_it_nodejs/myContacts/app.js
+++ do_it_nodejs/myContacts/app.js
@@ -4,8 +4,25 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Hello, Node!");
 });
+
 app.get("/contacts", (req, res) => {
     res.send("Contacts page");
+});
+
+app.post("/contacts/:id", (req, res) => {
+    res.send("Create Contacts");
+});
+
+app.get("/contacts/:id", (req, res) => {
+    res.send(`View Contact for ID: ${req.params.id}`);
+});
+
+app.put("/contacts/:id", (req, res) => {
+    res.send(`Update Contact for ID:" ${req.params.id}`);
+});
+
+app.delete("/contacts/:id", (req, res) => {
+    res.send(`Delete Contact for ID:" ${req.params.id}`);
 });
 
 app.listen(3000, () => {
